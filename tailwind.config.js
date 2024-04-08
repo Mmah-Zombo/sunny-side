8342d2a282a3b6fab86f9a39b9d96d20a3f815e1
M'mah Zombo <www.mariamzombo@gmail.com>
2024-04-08 01:22:49 +0000

    sunny side website

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        veryDarkblue: 'hsl(212, 27%, 19%)', 
+        veryDarkGrayishBlue: 'hsl(213, 9%, 39%)',
+        DarkGrayishBlue: 'hsl(232, 10%, 55%)',
+        GrayishBlue: 'hsl(210, 4%, 67%)',
+        SoftRed: 'hsl(7, 99%, 70%)',
+        Yellow: 'hsl(51, 100%, 49%)',
+        DarkSCyan: 'hsl(167, 40%, 24%)',
+        Darkblue: 'hsl(198, 62%, 26%)',
+        DarkMCyaN: 'hsl(168, 34%, 41%)',
+        ftr: 'rgb(117, 217, 217)'
+      }
+    },
+  },
+  plugins: [],
+}
